3bc9ba22c5f39905cbdacf14df29f8f028038296
shenhang <2322974451@qq.com>
2023-05-11 17:13:43 +0800

    注释

--- Assets/Demo/FockingFish/Scripts/FishFocking.cs
+++ Assets/Demo/FockingFish/Scripts/FishFocking.cs
@@ -74,8 +74,18 @@ namespace Fern.VFX
                 positionArray[i] = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) *
                                    5f;
                 float theta = Random.Range(-Mathf.PI, Mathf.PI);
+                // 这行代码的目的是生成一个随机的球坐标系角度 phi。球坐标系中的 phi 表示从正 z 轴到其所在点的连线与正 z 轴之间的夹角。
+                // 在这个代码中，phi 的随机范围是从 -1 到 1，即弧度制表示的 -90° 到 90°
+                // `Mathf.Asin` 函数用于计算给定数值的反正弦值，即求角度。将之前步骤生成的随机数作为输入，
+                // 计算结果为介于 -90° 到 90° 的一个随机角度，弧度制表示为 -π/2 到 π/2。
                 float phi = Mathf.Asin(Random.Range(-1f, 1f));
-                velocityArray[i] =
+
+                // Mathf.Cos(phi) * Mathf.Cos(theta)完整为 1 * Mathf.Cos(phi) * Mathf.Cos(theta)其中1为半径值
+                // 这个表达式经过两个分解步骤，首先处理φ角度以在xy平面上得到投影向量，然后根据θ角度计算x分量。这样我们就可以将
+                // 球坐标系中的向量转换到笛卡尔坐标系，从而得到速度向量的x分量。同样的方法也适用于计算y和z分量
+                // `sin(φ) = z / r`  --> `z = r * sin(φ)` --> `z = sin(φ)`
+                // `(speedRange.x + speedRange.y) * 0.5f`。这个表达式计算speedRange的范围的平均值，然后将其用于缩放速度向量。
+                velocityArray[i] = 
                     new Vector3(Mathf.Cos(phi) * Mathf.Cos(theta), Mathf.Cos(phi) * Mathf.Sin(theta), Mathf.Sin(phi)) *
                     (speedRange.x + speedRange.y) * 0.5f;
             }
